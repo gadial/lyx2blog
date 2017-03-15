833c06f68045522fab1bc541114464f8260a6e79
Gadi Aleksandrowicz <gadial@gmail.com>
2017-03-15 13:20:46 +0200

    Better enumerate\itemize handling, header handling, ensuremath warning, small refactoring

--- lyx2blog.rb
+++ lyx2blog.rb
@@ -10,11 +10,15 @@ TAGS = {
     "inputencoding{}"  => "",
     "textbf{}" => '<strong>\1</strong>',
     "href{}{}" => '<a href="\1">\2</a>',
+    "section{}" => '<h2>\1</h2>',
     ['\{\\\\beginL', '\\\\endL\}'] => '\1',
+    ['\\\\item', "(?=\n\\\\item)"] => '<li>\1</li>',
+    ['\\\\item', "(?=\n\\\\end\{)"] => '<li>\1</li>',
     ['\\\\begin\{itemize\}', '\\\\end\{itemize\}'] => '<ul>\1</ul>',
-    ['\\\\item', "\n"] => '<li>\1</li>',
+    ['\\\\begin\{enumerate\}', '\\\\end\{enumerate\}'] => '<ol>\1</ol>',
     "L{}" => '\1'
 }
+
 class String
     def basic_replacements
         result = dup
@@ -47,18 +51,40 @@ class String
     def parentheses_fix #relies on the fact that parentheses in mathmode are of the form \left( and \right)
         gsub(/(?<!\\left)\(|(?<!\\right)\)/,{'(' => ')', ')' => '('})
     end
+    def find_problems
+        scan(/^.*\\ensuremath.*$/){|s| puts "WARNING: ensuremath found in line '#{s}'"}
+        self
+    end
+    def get_content
+        self[/(?<=\\begin\{document\})(.*)(?=\\end\{document\})/m] ||  self
+    end
+    def all_changes
+        get_content.
+        find_problems.
+        basic_replacements.
+        parentheses_fix.
+        math_tag_replacements.
+        remove_comments.
+        replace_tags.
+        remove_linebreaks.
+        force_encoding("utf-8")
+    end
 end
 
-filename = File.basename(ARGV[0], ".lyx")
-`lyx --export latex #{filename}.lyx`
-text = File.read("#{filename}.tex", :encoding => 'ISO-8859-8').match(/(?<=\\begin\{document\})(.*)(?=\\end\{document\})/m)[0]
-File.open("#{filename}.blog", "w") do |f|
-    f.write text.
-    basic_replacements.
-    parentheses_fix.
-    math_tag_replacements.
-    remove_comments.
-    replace_tags.
-    remove_linebreaks.
-    force_encoding("utf-8")
-end                                  
+# text = '\begin{enumerate}
+# \item This is the
+# first item
+# \item and this is the second
+# item for this
+# \item third now bye bye
+# \end{enumerate}
+# '
+# 
+# puts text.all_changes    
+
+if __FILE__ == $0
+    filename = File.basename(ARGV[0], ".lyx")
+    `lyx --export latex #{filename}.lyx`
+    text = File.read("#{filename}.tex", :encoding => 'ISO-8859-8')
+    File.open("#{filename}.blog", "w"){|f| f.write text.all_changes}
+end
